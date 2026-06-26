2a296f0ca214f7264935612e6cced9e2a2078852
Manav Yadav <yadavmanav1304@gmail.com>
2026-06-26 17:04:53 +0530

    Add cron job to send health check requests every 14 minutes

--- backend/src/index.js
+++ backend/src/index.js
@@ -40,4 +40,5 @@ if (fs.existsSync(publicDir)) {
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
+   if (process.env.NODE_ENV === "production") job.start();
 })
