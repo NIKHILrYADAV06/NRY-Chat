d3b0bc1427c7a41aa43afb65fa95784cc7383f35
Manav Yadav <yadavmanav1304@gmail.com>
2026-06-27 12:10:36 +0530

    feat: implement chat functionality with message handling, user presence indicators, and UI components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,22 @@
 import { WallpaperProvider } from "./context/WallpaperContext";
 import { ThemeProvider } from "./context/ThemeContext";
-import { Route, Routes, Navigate } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import ChatPage from "./pages/ChatPage";
 import AuthPage from "./pages/AuthPage";
 import { useAuth } from "@clerk/react";
-import  PageLoader  from "./components/PageLoader";
+import PageLoader from "./components/PageLoader";
+import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
-import { useAuthStore } from "./store/authStore";
+
 import { Toaster } from "react-hot-toast";
 
 function App() {
+  const { isSignedIn, isLoaded } = useAuth();
 
-  const {isSignedIn, isLoaded} = useAuth();
+  // option 1
+  // const { checkAuth, isCheckingAuth, clearAuth } = useAuthStore();
 
+  // option 2 - better for performance
   const clearAuth = useAuthStore((state) => state.clearAuth);
   const checkAuth = useAuthStore((state) => state.checkAuth);
   const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
@@ -25,22 +29,21 @@ function App() {
   }, [checkAuth, clearAuth, isLoaded, isSignedIn]);
 
   if (!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
+
   return (
-    <>
     <ThemeProvider>
       <WallpaperProvider>
         <Routes>
-        <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
+          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
           <Route
             path="/auth"
             element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
           />
         </Routes>
         <Toaster />
-    </WallpaperProvider>
+      </WallpaperProvider>
     </ThemeProvider>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
